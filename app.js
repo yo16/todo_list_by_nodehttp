fe2abf07f618b4b7304be525692a77c3e526f7dc
yo16 <xxyo1xx@gmail.com>
2023-01-26 23:29:04 +0900

    add completed filter

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 'use strict'
-const exporess = require('express')
+const express = require('express')
 let todos = [
     { id: 1, title: 'ネーム', completed: false },
     { id: 2, title: '下書き', completed: true },
 ]
-const app = exporess()
+const app = express()
 
 // ToDo一覧の取得
-app.get('/api/todos', (req, res) => res.json(todos))
+app.get('/api/todos', (req, res) => {
+    if (!req.query.completed) {
+        res.json(todos)
+    }
+    // completedクエリパラメータを指定された場合は、ToDoをフィルタリング
+    const completed = req.query.completed === 'true'
+    return res.json(todos.filter(todo => todo.completed === completed))
+})
 
 app.listen(3000)
 
